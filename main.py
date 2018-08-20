2321c026a6a78c6494c79406d5c2c112a174156f
d032408 <bernd.kuespert@sap.com>
2018-08-20 21:48:07 +0200

    Skip updating configmap if no devices were found.

--- main.py
+++ main.py
@@ -136,6 +136,11 @@ if __name__ == '__main__':
     while True:
         devices = mydiscovery.get_devices()
         logging.debug("Devices: %s", devices)
-        write_configmap(myconfig, devices)
+
+        if len(devices)==0:
+            logging.info("No devices found. Skipping update of configmap.")
+        else:
+            write_configmap(myconfig, devices)
   
+        logging.info("Sleeping for %s seconds ...", myconfig['refresh_interval'])
         time.sleep(myconfig['refresh_interval'])
