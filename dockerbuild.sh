7fc375a88e25150e4e99e134f365ca9eeac307d6
d032408 <bernd.kuespert@sap.com>
2018-11-23 16:23:45 +0100

    Bump to current docker file version

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,2 +1,2 @@
-docker build . -t hub.global.cloud.sap/monsoon/arista-service-discovery:v0.0.3
-docker push hub.global.cloud.sap/monsoon/arista-service-discovery:v0.0.3
+docker build . -t hub.global.cloud.sap/monsoon/arista-service-discovery:v0.0.8
+docker push hub.global.cloud.sap/monsoon/arista-service-discovery:v0.0.8
