384bfd9862350c1788b73e0952591fe7280588a8
d032408 <bernd.kuespert@sap.com>
2020-09-08 18:10:54 +0200

    correct registry patch

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -3,5 +3,5 @@ version="v0.1.0"
 docker build . -t hub.global.cloud.sap/monsoon/arista-service-discovery:${version}
 docker push hub.global.cloud.sap/monsoon/arista-service-discovery:${version}
 
-docker build . -t keppel.eu-de-1.cloud.sap/monsoon/arista-service-discovery:${version}
-docker push keppel.eu-de-1.cloud.sap/monsoon/arista-service-discovery:${version}
+docker build . -t keppel.eu-de-1.cloud.sap/ccloud/arista-service-discovery:${version}
+docker push keppel.eu-de-1.cloud.sap/ccloud/arista-service-discovery:${version}
